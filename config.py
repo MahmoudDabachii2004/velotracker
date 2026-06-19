d9a8f7cfe08e5adef639df33cf8c3f66aa9afb4e
MahmoudDabachii2004 <mahmouddabachi2004@gmail.com>
2026-06-19 12:18:30 -0400

    config: reduce BLE notification interval to 250ms for smoother updates

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ TRACKING_FIT_WINDOW = 150
 # to 28 bytes. If the name is >10 chars, bless may drop the service UUIDs
 # from the advertisement, so apps like MyWhoosh can't find the device.
 BLE_DEVICE_NAME = "VeloTrack"  # 9 chars - safe
-BLE_NOTIFY_INTERVAL_SEC = 0.5  # 2Hz heartbeat (PeloMon-proven value)
+BLE_NOTIFY_INTERVAL_SEC = 0.25  # 4Hz heartbeat for smoother updates and fewer drops
 WHEEL_TO_CRANK_RATIO = 2.0          # 1 pedal rev = 2 wheel revs (50x11)
 WHEEL_CIRCUMFERENCE_M = 2.105       # 700x25C road tire
 
